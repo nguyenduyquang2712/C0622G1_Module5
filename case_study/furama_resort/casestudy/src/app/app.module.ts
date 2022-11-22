5540766b8e2a0802935ff2b8419df91a72d7898c
nguyenduyquang2712 <quangkg8@gmail.com>
2022-11-22 15:08:01 +0700

    update

--- case_study/furama_resort/casestudy/src/app/app.module.ts
+++ case_study/furama_resort/casestudy/src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { ListFacilityComponent } from './facilities/list-facility/list-facility.component';
+import { EditFacilityComponent } from './facilities/edit-facility/edit-facility.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavigationComponent
+    NavigationComponent,
+    ListFacilityComponent,
+    EditFacilityComponent,
   ],
   imports: [
     BrowserModule
