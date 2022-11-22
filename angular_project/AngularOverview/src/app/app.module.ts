ea7efce944c8e84a6965ddb9c770b42b37622fc3
nguyenduyquang2712 <quangkg8@gmail.com>
2022-11-22 14:28:07 +0700

    update

--- angular_project/AngularOverview/src/app/app.module.ts
+++ angular_project/AngularOverview/src/app/app.module.ts
@@ -2,15 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { CalculatorComponent } from './calculator/calculator.component';
+import { CalculatorComponent } from './ss3_angular_overview/calculator/calculator.component';
 import {FormsModule} from "@angular/forms";
-import { AngularColorPickerAppComponent } from './angular-color-picker-app/angular-color-picker-app.component';
+import { AngularColorPickerAppComponent } from './ss3_angular_overview/angular-color-picker-app/angular-color-picker-app.component';
+import { ArticleComponent } from './ss4_angular_template/article/article.component';
+import { LikeComponent } from './ss4_angular_template/like/like.component';
+import { NavbarComponent } from './ss4_angular_template/navbar/navbar.component';
+import { FooterComponent } from './ss4_angular_template/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CalculatorComponent,
     AngularColorPickerAppComponent,
+    ArticleComponent,
+    LikeComponent,
+    NavbarComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
